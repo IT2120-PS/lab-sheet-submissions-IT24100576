a0dc7c140e738c915f20810ff81b4b29f2c00d54
IT24100576 <it24100576@my.sliit.lk>
2025-10-02 00:27:13 +0530

    IT24100576

---
+++ Lab Sheet 09/IT24100576.R
@@ -0,0 +1,12 @@
+setwd("C:\\Users\\User\\Desktop\\IT24100576")
+getwd()
+#1
+set.seed(123)
+bake_time<-rnorm(25,mean=45,sd=2)
+print(bake_time)
+
+#ii
+t.test(bake_time,mu=46,alternative = "less",conf.level = 0.95)
+print(t.test)
+
+
